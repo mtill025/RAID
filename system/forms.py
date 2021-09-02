bf653cc769f62dfe0c9b637f6a2530a07561161d
mtill025 <mjtill025@gmail.com>
2021-09-01 22:39:49 -0500

    Add RegisterUser form class

--- system/forms.py
+++ system/forms.py
@@ -9,6 +9,13 @@ class LoginForm(FlaskForm):
     login_submit = SubmitField("Login")
 
 
+class RegisterUserForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    role = SelectField("Role", choices=[(1, 'Admin'), (2, 'User')])
+    register_submit = SubmitField("Add User")
+
+
 class SearchForm(FlaskForm):
     type = SelectField("Search by", choices=['Asset Tag', 'Serial Number'])
     identifier = StringField("Number", validators=[DataRequired()])
